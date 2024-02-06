5ec94727897fcaad3afdb057b8586a120980d8e7
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-06 12:46:34 -0300

    feat(git): creating the Container component
    Starting the creation of the Container component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import Container from './components/Container';
 
 function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Container></Container>
+    </>
   );
 }
 
