f102a1f2eb475de6aa349b718e46163ee6cf0f18
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-07 01:38:25 -0300

    style(git): finalizing styles in the Card component

--- src/components/Assessment/index.tsx
+++ src/components/Assessment/index.tsx
@@ -1,13 +1,21 @@
+import styled from "styled-components";
 import IProfessional from "../../types/IProfessional";
 import Card from "./Card";
 
+const CardSection = styled.section`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    width: 100%;
+`;
+
 function Assessment({ profissionais }: { profissionais: IProfessional[] | null }) {
   return (
-    <section>
+    <CardSection>
       {profissionais?.map((profissional) => {
         return <Card profissional={profissional} />
       })}
-    </section>
+    </CardSection>
   )
 }
 
