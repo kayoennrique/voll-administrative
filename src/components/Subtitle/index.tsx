02cf1761175785b51253c85e7dfd6c8a757f95fd
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-07 03:12:18 -0300

    fix(git): update

--- src/components/Subtitle/index.tsx
+++ src/components/Subtitle/index.tsx
@@ -1,9 +1,9 @@
 import styled from "styled-components";
 
-const Subtitulo = styled.h3`
-color: var(--azul-escuro);
-font-weight: 700;
-font-size: 18px;
-`
+const Subtitle = styled.h3`
+    color: var(--dark-blue);
+    font-weight: 700;
+    font-size: 18px;
+`;
 
-export default Subtitulo;
+export default Subtitle;
