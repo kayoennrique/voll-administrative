f0f8251b18648486030038c403d6f2aa8eb83d79
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-07 01:01:34 -0300

    style(git): finalizing style in the Graphic component

--- src/components/Graphic/index.tsx
+++ src/components/Graphic/index.tsx
@@ -2,6 +2,7 @@ import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from "recharts";
 import IQuery from "../../types/IQuery";
 import IProfessional from "../../types/IProfessional";
 import useDataGraphic from "./useDataGraphic";
+import styled from "styled-components";
 
 interface Props {
   profissionais: IProfessional[] | null,
@@ -13,20 +14,27 @@ interface IData {
   consultas: number
 }
 
+const StylizedSection = styled.section`
+background-color: var(--white);
+border-radius: 16px;
+`
+
 function Graphic({ profissionais, consultas }: Props) {
   let dados: Array<IData> = useDataGraphic({ profissionais, consultas });
   return (
-    <ResponsiveContainer width="100%" height={350}>
-      <BarChart
-        layout="vertical"
-        data={dados}
-        margin={{ top: 25, right: 40, left: 40, bottom: 20 }}
-      >
-        <XAxis type="number"></XAxis>
-        <YAxis type="category" dataKey="nome"></YAxis>
-        <Bar dataKey="consultas" fill="#083860" barSize={30}></Bar>
-      </BarChart>
-    </ResponsiveContainer>
+    <StylizedSection>
+      <ResponsiveContainer width="100%" height={350}>
+        <BarChart
+          layout="vertical"
+          data={dados}
+          margin={{ top: 25, right: 40, left: 40, bottom: 20 }}
+        >
+          <XAxis type="number"></XAxis>
+          <YAxis type="category" dataKey="nome"></YAxis>
+          <Bar dataKey="consultas" fill="#083860" barSize={30}></Bar>
+        </BarChart>
+      </ResponsiveContainer>
+    </StylizedSection>
   );
 }
 
