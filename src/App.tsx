82a8578a2ddbebc4a8ba00a5402b572befea2ec5
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-07 00:34:33 -0300

    feat(git): creating graphics components
    Applying graphics components with recharts

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import Title from './components/Title';
 import Footer from './components/Footer';
 import Tablle from './components/Table';
 import useQueryData from './useQueryData';
+import Graphic from './components/Graphic';
 
 function App() {
   const { dados, erro } = useQueryData();
@@ -14,6 +15,7 @@ function App() {
       <Container>
         <Title>Area Administrativa</Title>
         <Tablle consultas={dados} />
+        <Graphic />
       </Container>
       <Footer />
     </>
